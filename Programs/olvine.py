5b4d915beb807d62ce01d68d09221bf231355c31
Olivine George <olivinegeorge@gmail.com>
2020-11-25 13:15:04 +0300

    I have added a new file named olvine.py to Programs

---
+++ Programs/olvine.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+lang = input("What programming language are you familiar with?")
+end_text = "Goodbye " + name + " pleasure knowing you as we code " + lang + "together"
+print(end_text)
+
